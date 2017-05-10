e8133eb1f1c3b3791435fbb630b4db5cbbeb6764
Sean Carroll <carroll.seann@gmail.com>
2017-05-10 14:01:09 +0100

    test

--- Easy Challanges/Mars Lander - Episode 1.java
+++ Easy Challanges/Mars Lander - Episode 1.java
@@ -12,6 +12,7 @@ class Player {
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
         int surfaceN = in.nextInt(); // the number of points used to draw the surface of Mars.
+        
         for (int i = 0; i < surfaceN; i++) {
             int landX = in.nextInt(); // X coordinate of a surface point. (0 to 6999)
             int landY = in.nextInt(); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
@@ -19,6 +20,7 @@ class Player {
 
         // game loop
         while (true) {
+            
             int X = in.nextInt();
             int Y = in.nextInt();
             int hSpeed = in.nextInt(); // the horizontal speed (in m/s), can be negative.
